66b4dfe036c0a09df2945aae7dea7d5ae03f9e15
Chris Evan <cevan1835@gmail.com>
2020-07-06 13:40:22 +0700

    #1

--- setup_docker.sh
+++ setup_docker.sh
@@ -15,16 +15,3 @@ add-apt-repository \
    stable"
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
-
-#intsall docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-#install nvidia-docker
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
-
-apt-get update
-apt-get install -y nvidia-container-toolkit
-systemctl restart docker
---
+++ setup_nvidia_docker.sh
@@ -0,0 +1,8 @@
+#install nvidia-docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
+
+apt-get update
+apt-get install -y nvidia-container-toolkit
+systemctl restart docker
